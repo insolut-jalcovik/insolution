0d8d1d38920eace30642525df3a7c3ce4c414c3b
Jan Jalcovik <jan.jalcovik@in-solut.com>
2016-10-04 16:57:11 +0200

    queens_problem optimized

--- queens.php
+++ queens.php
@@ -9,17 +9,8 @@ for ($i=0; $i<$queens; $i++){
 
 $GLOBALS['solutions'] = 0;
 
-function vykresli($currentArray){
-	print "\n";
-	for ($i=0; $i<sizeof($currentArray); $i++){
-		for ($j=0; $j<sizeof($currentArray); $j++){
-			print " ".$currentArray[$i][$j]." ";
-		}
-		print "\n";
-	}	
-}
-
-function checkPosition($row, $column, $currentArray, $queens){
+function checkPositionPossibility($row, $column, $currentArray, $queens){
+	//check rows and columns
 	for ($i=0; $i < ($queens-1); $i++){
 		if ($currentArray[$row][$i] == 1){
 			return 0;
@@ -29,37 +20,17 @@ function checkPosition($row, $column, $currentArray, $queens){
 		}
 	}
 	
-	$m = $row; $n = $column;
-	while (($m >= 0)&&($n >= 0)){
-		if ($currentArray[$m][$n] == 1){
-			return 0;
+	//checking diagonals 
+	for ($i=0; $i<$queens; $i++){
+		for ($j=0; $j<$queens; $j++){
+			if ($currentArray[$i][$j] == 1){
+				if (((abs($row-$i))/(abs($column-$j))) == 1){
+					return 0;
+				}
+			}
 		}
-		$m--; $n--; 
 	}
 	
-	$m = $row; $n = $column;
-	while (($m <= ($queens-1))&&($n >= 0)){
-		if ($currentArray[$m][$n] == 1){
-			return 0;
-		}
-		$m++; $n--;
-	}
-	
-	$m = $row; $n = $column;
-	while (($m <= ($queens-1))&&($n <= ($queens-1))){
-		if ($currentArray[$m][$n] == 1){
-			return 0;
-		}
-		$m++; $n++;
-	}
-	
-	$m = $row; $n = $column;
-	while (($m >= 0)&&($n <= ($queens-1))){
-		if ($currentArray[$m][$n] == 1){
-			return 0;
-		}
-		$m--; $n++;
-	}
 	return 1;
 }
 
@@ -70,21 +41,20 @@ function find_previous_column($row){
 
 
 function recursion($m, $n, $currentArray, $queens){
-	$currentArray[$m][$n-1] = 0;
-	
  	for ($i=$m; $i<$queens; $i++){
  		for ($j=$n; $j<$queens; $j++){
  			if(($i>0) && false === find_previous_column($currentArray[$i-1])){
  				return;
  			}
-			if(checkPosition($i, $j, $currentArray, $queens) == 1){
-				$currentArray[$i][$j] = 1;
-				if ($i == ($queens-1)){
+			if(checkPositionPossibility($i, $j, $currentArray, $queens) == 1){
+				if ($i == ($queens-1)){ // we found another solution
 					$GLOBALS['solutions']++;
 					return;
 				}
-				recursion($i+1, 0, $currentArray, $queens);		
- 				recursion($i, $j+1, $currentArray, $queens);
+				
+				recursion($i, $j+1, $currentArray, $queens);
+				$currentArray[$i][$j] = 1;
+				recursion($i+1, 0, $currentArray, $queens);
  				return;
 			}
  		}
